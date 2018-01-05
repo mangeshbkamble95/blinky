a8792a286e8632ecc468a64a17f8365b35e32eaf
mangeshbkamble95 <35131502+mangeshbkamble95@users.noreply.github.com>
2018-01-05 13:05:27 +0530

    new project

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * File:   main.c
+ * Author: MANGESH B. KAMBLE
+ *
+ * Created on 29 December, 2017, 6:55 PM
+ */
+
+
+#include <xc.h>
+#include "main.h"
+void init(void)
+{
+    TRISA=0X10;
+    PORTA=0X00;
+
+
+
+}
+void main(void)
+{
+    init();
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
